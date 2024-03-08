5d4d082af29969c6fb193d2d553a7bba4a95b1a1
Wanessa Lopes <wanessaalopes@gmail.com>
2024-03-08 16:44:26 -0700

    import fonts to assets

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,17 @@
+import "./SeacrBar.scss";
+import Search from "../../assets/magnifying-glass-solid.svg"
 function SearchBar () {
     return (
       <section className="search">
         <form className="search-bar">
           <input
             type="text"
+            className="search-bar__input"
             placeholder="Search for a movie"
           />
-          <button className="btn" type="submit">Search</button>
+          <button className="btn" type="submit">
+            <img src={ Search } alt="search logo"/>
+          </button>
         </form>
       </section>
     );
