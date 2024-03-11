57b2e6a10e5833d9208c7633a502e0b26f9c6682
Wanessa Lopes <wanessaalopes@gmail.com>
2024-03-10 21:45:40 -0700

    Add margin and heigth on input field

--- src/components/AccountForm/AccountForm.js
+++ src/components/AccountForm/AccountForm.js
@@ -1,16 +1,64 @@
+import React, { useState } from "react";
+import "./AccountForm.scss";
+
 function AccountForm() {
+  const inicialFormData = {
+    username: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  }
+
+  const [ formData, setFormData] = useState(inicialFormData);
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();  
+  }
+
+
     return (
       <section>
-        <div>
-          <form>
+        <div className="section">
+          <form className="section__form" onSubmit={handleSubmit}>
             <label></label>
-            <input type="text" placeholder="Username" />
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+              placeholder="Username"
+            />
             <label></label>
-            <input type="text" placeholder="Email" />
+            <input
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              placeholder="Email"
+            />
             <label></label>
-            <input type="text" placeholder="Password" />
+            <input
+              type="text"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              placeholder="Password"
+            />
             <label></label>
-            <input type="text" placeholder="Confirm Password" />
+            <input
+              type="text"
+              name="confirm_password"
+              value={formData.confirm_password}
+              onChange={handleInputChange}
+              placeholder="Confirm Password"
+            />
             <button>Register</button>
           </form>
         </div>
--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,13 +1,9 @@
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
 import AccountForm from "../../components/AccountForm/AccountForm";
 
 function Account() {
   return (
     <section>
-      <Header />
       <AccountForm/>
-      <Footer />
     </section>
   );
 }
