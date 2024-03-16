784a7fc8fc2950a90561842cd1e9ce9dca2ab355
Wanessa Lopes <wanessaalopes@gmail.com>
2024-03-16 12:07:32 -0700

    Apply styles on search bar

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-function SearchResults({ query }) {
+function SearchResults({ query, results }) {
+  const resultsArray = results || [];
+  console.log("Query:", query);
+  console.log("Results:", results);
+
   return (
     <section>
       <div>
         <p>Results for "{query} "</p>
       </div>
+      {resultsArray.length > 0 ? (
+        <div>
+          <ul>
+            {resultsArray.map((result) => (
+              <li key={result.id}>
+                <img src={result.poster_path} alt={result.title} />
+                <p>{result.title}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>No results found.</p>
+      )}
     </section>
   );
 }
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,5 +1,5 @@
 import SearchResults from "../../components/SearchResults/SearchResults";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router";
 import Header from "../../components/Header/Header";
@@ -7,6 +7,8 @@ import Footer from "../../components/Footer/Footer";
 
 function Search() {
   const { query } = useParams();
+  const [searchResults, setSearchResults] = useState([]);
+
   useEffect(() => {
     axios
       .get(`http://localhost:8080/api/movies/search`, {
@@ -17,6 +19,7 @@ function Search() {
       })
       .then((response) => {
         //   setFormData(response);
+        setSearchResults(response.data.results);
         console.log(response);
       })
       .catch((error) => {
@@ -26,7 +29,7 @@ function Search() {
   return (
     <section>
       <Header />
-      <SearchResults query={query} />
+      <SearchResults query={query} results={searchResults} />
       <Footer />
     </section>
   );
